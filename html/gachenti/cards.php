bf7926576a9f18264192cede4343031b50d4cd59
lucfelia <lucia.puga@estudiant.enti.cat>
2025-11-18 22:33:40 +0100

    Ejercicio: Registro y listado de cartas

--- html/gachenti/cards.php
+++ html/gachenti/cards.php
@@ -6,12 +6,36 @@ openHTML("Listado de cartas", "cards");
 
 writeHeader();
 
-$contenido = <<<EOD
-<section>
-	<h2>Listado de cartas</h2>
-	<p>Aquí irán las cartas chachis obtenidas de la base de datos.</p>
-</section>
+$query = <<<EOD
+SELECT cards.id_card, card_templates.card, cards.price FROM cards LEFT JOIN card_templates ON cards.id_card_template = card_templates.id_card_template;
 EOD;
+
+$conn = mysqli_connect("localhost", "enti", "enti", "gachenti_db");
+if (!$conn) {
+	die("Error DB 1: Error en la conexión");
+}
+
+$result = mysqli_query($conn, $query);
+if (!$result) {
+	die("Error DB 2: Error al realizar la petición");
+}
+
+if (mysqli_num_rows($result) <= 0) {
+	die("Error 1: No hay cartas");
+}
+
+$contenido = "<section>\n";
+$contenido .= "\t<h2>Listado de cartas</h2>\n";
+$contenido .= "\t<ol>\n";
+
+while ($card = mysqli_fetch_assoc($result)) {
+	$contenido .= "\t\t<li><strong>CardID:</strong> {$card["id_card"]}; ";
+	$contenido .= "<strong>Card:</strong> {$card["card"]}; ";
+	$contenido .= "<strong>Price:</strong> {$card["price"]}</li>\n";
+}
+
+$contenido .= "\t</ol>\n";
+$contenido .= "</section>\n";
 
 writeMain($contenido);
 
